79e3b94de8931e3fa55a293a7c5e8795a0babf15
unknown <dimashishov2000@gmail.com>
2022-04-16 17:22:57 +0300

    created business card generator

--- src/BusinessCard.js
+++ src/BusinessCard.js
@@ -1,7 +1,14 @@
 
 class BusinessCard {
-  constructor() {
-    console.log('new BusinessCard!')
+  constructor(cardNumber, expirationDate, securityNumber, purseId) {
+    this.purseId = purseId;
+    this.cardNumber = cardNumber;
+    this.expirationDate = expirationDate;
+    this.securityNumber = securityNumber;
+  }
+
+  isExpired(date) {
+    return date > this.expirationDate;
   }
 }
 
